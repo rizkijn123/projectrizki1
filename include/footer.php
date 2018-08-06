153429ae0e65560cca8fce48295eb69df040f49f
rizkijn123 <42140040+rizkijn123@users.noreply.github.com>
2018-08-06 18:28:11 +0700

    Add files via upload

---
+++ include/footer.php
@@ -0,0 +1,3 @@
+	<div class="footer-wrap bg-white pd-20 mb-20 border-radius-5 box-shadow">
+		VIP BloodLust - Redesign Template By <a href="#">Rizki Jaelani N & Restu Fauzi</a>
+	</div>
---
+++ include/header.php
@@ -0,0 +1,55 @@
+
+	<div class="pre-loader"></div>
+	<div class="header clearfix">
+		<div class="header-right">
+			<div class="">
+				<a href="index.php">
+				</a>
+			</div>
+			<div class="menu-icon">
+				<span></span>
+				<span></span>
+				<span></span>
+				<span></span>
+			</div>
+			<div class="user-info-dropdown">
+				<div class="dropdown">
+					<a class="dropdown-toggle" href="#" role="button" data-toggle="dropdown">
+						<span class="user-icon"><i class="fa fa-user-o"></i></span>
+						<span class="user-name">Johnny Brown</span>
+					</a>
+					<div class="dropdown-menu dropdown-menu-right">
+						<a class="dropdown-item" href="logout.php"><i class="fa fa-sign-out" aria-hidden="true"></i> Log Out</a>
+					</div>
+				</div>
+			</div>
+			<div class="user-notification">
+				<div class="dropdown">
+					<a class="dropdown-toggle no-arrow" href="#" role="button" data-toggle="dropdown">
+						<i class="fa fa-bell" aria-hidden="true"></i>
+						<span class="badge notification-active"></span>
+					</a>
+					<div class="dropdown-menu dropdown-menu-right">
+						<div class="notification-list mx-h-350 customscroll">
+							<ul>
+								<li>
+									<a href="#">
+										<img src="" alt="">
+										<h3 class="clearfix">Iqbal <span>3 mins ago</span></h3>
+										<p>Selamat Bergabung</p>
+									</a>
+								</li>
+								<li>
+									<a href="#">
+										<img src="" alt="">
+										<h3 class="clearfix">Rizki Jaelani <span>3 mins ago</span></h3>
+										<p>Proses Order Lancar</p>
+									</a>
+								</li>
+							</ul>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
